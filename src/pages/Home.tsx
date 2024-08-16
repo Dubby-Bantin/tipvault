41a3323c36071d684888837a7ab80c1bbbb26b24
Dubby-Bantin <dubbybantin@gmail.com>
2024-08-16 18:43:17 +0100

    stopped using json-server|started using localstorage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,22 @@
+import { useEffect } from "react";
 import { Banner, Featured, Categories, LatestTips } from "../utils/exports";
+import { tips } from "../utils/tips";
+interface TipFormData {
+  id: string;
+  title: string;
+  description: string;
+  language: string;
+  tags: string[];
+  created_at?: string; // Added optional created_at field
+}
+const saveTips = (tips: TipFormData[]) => {
+  localStorage.setItem("tips", JSON.stringify(tips));
+};
+
 const Home = () => {
+  useEffect(() => {
+    saveTips(tips);
+  });
   return (
     <div>
       <Banner />
