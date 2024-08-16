27f4d6f335adf894a4cd7b201623f71609324190
Dubby-Bantin <dubbybantin@gmail.com>
2024-08-16 20:05:27 +0100

    stopped using json-server|started using localstorage

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -5,24 +5,28 @@ const SkeletonCards = ({ cards }: { cards: number }) =>
     .fill(0)
     .map((_, i) => (
       <SkeletonTheme baseColor="#111827" highlightColor="#60a5fa" key={i}>
-        <div className="relative border border-opacity-10 border-primary p-4 rounded-lg shadow-lg w-full max-w-full sm:max-w-[35rem] mx-auto">
-          <div className="flex items-center justify-between">
-            <Skeleton height="1rem" width="60%" /> {/* Ensure enough width */}
-            <Skeleton height="30px" borderRadius={"50%"} width="30px" />
+        <div className="border border-opacity-10 border-primary relative p-4 rounded-lg shadow-lg w-full max-w-[35rem]">
+          {/* Skeleton for Title and Badge */}
+          <div className="flex justify-between items-center mb-2">
+            <Skeleton width={200} height={20} />
+            <Skeleton circle={true} width={20} height={20} />
           </div>
-          <div className="flex flex-col gap-2">
-            <Skeleton height="10px" width="50%" /> {/* Adjusted for better fit */}
-            <Skeleton height="20px" width="100%" className="mb-2" /> {/* Full width for better rendering */}
+
+          {/* Skeleton for Description */}
+          <Skeleton width={50} height={15} className="mb-4" />
+          <Skeleton height={15} className="mb-4" />
+
+          {/* Skeleton for Tags */}
+          <div className="flex flex-wrap gap-2 mb-4">
+            <Skeleton width={60} height={20} />
+            <Skeleton width={60} height={20} />
+            <Skeleton width={60} height={20} />
           </div>
-          <div className="flex justify-between items-center">
-            <div className="flex gap-4">
-              <Skeleton height="10px" width="25%" /> {/* Adjusted widths */}
-              <Skeleton height="10px" width="25%" />
-              <Skeleton height="10px" width="25%" />
-            </div>
-            <div>
-              <Skeleton height="10px" width="100%" /> {/* Full width for footer element */}
-            </div>
+
+          {/* Skeleton for Date and Category */}
+          <div className="flex justify-between">
+            <Skeleton width={100} height={15} />
+            <Skeleton width={100} height={15} />
           </div>
         </div>
       </SkeletonTheme>
