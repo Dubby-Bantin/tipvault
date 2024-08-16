881e021d7b03d9995d10d545b2ea53791b6fff41
Dubby-Bantin <dubbybantin@gmail.com>
2024-08-16 15:21:26 +0100

    changed the crud-directory location

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Footer from "../components/Footer";
 import Tips from "../pages/Tips";
-import Create from "../components/crud/Create";
+import Create from "../pages/crud/Create";
 
 const AppRoutes = () => {
   return (
@@ -12,7 +12,6 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tips" element={<Tips />} />
-        <Route path="/tips/:id" element={<Tips />} />
         <Route path="/create" element={<Create />} />
       </Routes>
       <Footer />
